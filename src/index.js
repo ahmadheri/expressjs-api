05e4006a955d8b5494d4b5cede721800b18197aa
ahmadheri <dev.ahmadheri@gmail.com>
2024-04-01 00:44:18 +0800

    add get response

--- src/index.js
+++ src/index.js
@@ -4,6 +4,24 @@ const app = express();
 
 const PORT = process.env.PORT || 3000;
 
+app.get('/', (req, res) => {
+  res.status(201).send({ msg: "hello"})
+});
+
+app.get('/api/users', (req, res) => {
+  res.send([
+    { id: 1, username: "Heri"},
+    { id: 2, username: "Ahmad"},
+    { id: 3, username: "Darwis"}
+  ]);
+});
+
+app.get('/api/products', (req, res) => {
+  res.send([
+    { id: 1, name: "Samsung Galaxy M33", price: 2999000}
+  ]);
+});
+
 app.listen(PORT, () => {
   console.log(`Running on port ${PORT}`)
 });

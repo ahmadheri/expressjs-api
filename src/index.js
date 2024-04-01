98eb129e87cbfbb3cbe0536874f5af87fc91af85
ahmadheri <dev.ahmadheri@gmail.com>
2024-04-01 16:38:12 +0800

    add route parameter

--- src/index.js
+++ src/index.js
@@ -1,20 +1,33 @@
-import express from 'express';
+import express, { response } from 'express';
 
 const app = express();
 
 const PORT = process.env.PORT || 3000;
+
+const mockUsers = [
+  { id: 1, username: "Heri"},
+  { id: 2, username: "Ahmad"},
+  { id: 3, username: "Darwis"}
+];
 
 app.get('/', (req, res) => {
   res.status(201).send({ msg: "hello"})
 });
 
 app.get('/api/users', (req, res) => {
-  res.send([
-    { id: 1, username: "Heri"},
-    { id: 2, username: "Ahmad"},
-    { id: 3, username: "Darwis"}
-  ]);
+  res.send(mockUsers);
 });
+
+app.get('/api/users/:id', (req, res) => {
+  console.log(req.params)
+  const parsedId = parseInt(req.params.id);
+  console.log(parsedId)
+  if (isNaN(parsedId)) return res.status(400).send({ msg: "bad request"});
+  
+  const findUser = mockUsers.find((user) => user.id == parsedId);
+  if (!findUser) return res.sendStatus(404);
+  return res.send(findUser);
+})
 
 app.get('/api/products', (req, res) => {
   res.send([
